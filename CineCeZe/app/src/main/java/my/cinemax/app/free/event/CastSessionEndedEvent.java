06eac137de160d95eb411a75fbc9a21e664984cf
MovieAddict88 <ronatorrejos1@gmail.com>
2025-07-18 18:33:17 +0800

    Add files via upload

--- CineCeZe/app/src/main/java/my/cinemax/app/free/event/CastSessionEndedEvent.java
+++ CineCeZe/app/src/main/java/my/cinemax/app/free/event/CastSessionEndedEvent.java
@@ -1,1 +1,27 @@
+package my.cinemax.app.free.event;
 
+/**
+ * Created by Tamim on 28/09/2019.
+
+ */
+
+public class CastSessionEndedEvent {
+
+    private long sessionRemainingTime;
+
+    public CastSessionEndedEvent() {
+
+    }
+
+    public CastSessionEndedEvent(long sessionRemainingTime) {
+        this.sessionRemainingTime = sessionRemainingTime;
+    }
+
+    public long getSessionRemainingTime() {
+        return sessionRemainingTime;
+    }
+
+    public void setSessionRemainingTime(long sessionRemainingTime) {
+        this.sessionRemainingTime = sessionRemainingTime;
+    }
+}
---
+++ CineCeZe/app/src/main/java/my/cinemax/app/free/event/CastSessionStartedEvent.java
@@ -0,0 +1,13 @@
+package my.cinemax.app.free.event;
+
+/**
+ * Created by Tamim on 28/09/2019.
+
+ */
+
+public class CastSessionStartedEvent {
+
+    public CastSessionStartedEvent() {
+
+    }
+}
